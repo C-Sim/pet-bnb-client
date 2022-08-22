e4471ebed7372a95bbe3bf3475d308b15bae33a8
surajverma2587 <surajverma2587@gmail.com>
2022-08-22 21:39:09 +0100

    fix sign up page

--- src/containers/SignUpPage.js
+++ src/containers/SignUpPage.js
@@ -8,7 +8,7 @@ export const SignUpPage = () => {
   const isMobile = useMediaQuery("(max-width:900px)");
 
   return (
-    <Grid container sx={{ minHeight: "100vh" }}>
+    <Grid container sx={{ minHeight: "95vh" }}>
       {!isMobile && (
         <Grid item md={6}>
           <ImageWithBackground imageUrl="https://images.unsplash.com/photo-1625316708582-7c38734be31d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
